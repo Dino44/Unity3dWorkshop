f2051f8ec5fe7d7484b95faa9b0b1f4df0f7d395
Dino44 <dino@corinaandco.com>
2016-08-26 08:51:18 +0100

    we made a maze

---
+++ Assets/Dude.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu]
+public class Dude : ScriptableObject{
+	public int age;
+	public string nationality;
+	public string job;
+
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript {
+void Start(){Debug . Log("start");}
+	void Update (){
+		Debug.LogFormat ("Time={0}", Time.time);
+	}
+}
